64788ee3d05714561b551399851c3ecdf64bdcdd
Mateus Bez Fontana <mateus.bezfontana@gmail.com>
2020-06-28 09:07:06 -0300

    feat: validate appointment date

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,20 +1,38 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppoinmtmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppoinmtmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked.' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
-  appointments.push(appointments);
+  appointments.push(appointment);
 
   return response.json(appointment);
 });
