93d465c06aee4277605622c59e0af11e44750b84
Mateus Bez Fontana <mateus.bezfontana@gmail.com>
2020-07-19 15:38:44 -0300

    style: remove unused code

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,5 +1,3 @@
-import { isEqual } from 'date-fns';
-
 import { EntityRepository, Repository } from 'typeorm';
 
 import Appointment from '../models/Appointment';
@@ -7,10 +5,6 @@ import Appointment from '../models/Appointment';
 @EntityRepository(Appointment)
 class AppointmentsRepository extends Repository<Appointment> {
   public async findByDate(date: Date): Promise<Appointment | null> {
-    // const findAppointment = this.appointments.find(appointment =>
-    //   isEqual(date, appointment.date),
-    // );
-
     const findAppointment = await this.findOne({
       where: { date },
     });
