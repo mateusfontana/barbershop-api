834d362fc64176ca72ed3a58245b84ca6fce28d2
Mateus Bez Fontana <mateus.bezfontana@gmail.com>
2020-07-19 15:35:56 -0300

    fix: change id type on tables appointments and users

--- src/database/migrations/1594588171367-CreateUsers.ts
+++ src/database/migrations/1594588171367-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1594588171367 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
