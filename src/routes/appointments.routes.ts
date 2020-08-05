14774b468ad5e89adf19f2ecec65b6ff2185f7a8
Mateus Bez Fontana <mateus.bezfontana@gmail.com>
2020-08-05 07:55:14 -0300

    refactor: create global error handler

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -18,22 +18,18 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { providerId, date } = request.body;
+  const { providerId, date } = request.body;
 
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService();
 
-    const appointment = await createAppointment.execute({
-      date: parsedDate,
-      providerId,
-    });
+  const appointment = await createAppointment.execute({
+    date: parsedDate,
+    providerId,
+  });
 
-    return response.json(appointment);
-  } catch (error) {
-    return response.status(400).json({ error: error.message });
-  }
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import 'reflect-metadata';
 import './database';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+import AppError from './errors/AppError';
 import routes from './routes';
 import uploadConfig from './config/upload';
 
@@ -10,6 +13,22 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('🚀 Server started!');
