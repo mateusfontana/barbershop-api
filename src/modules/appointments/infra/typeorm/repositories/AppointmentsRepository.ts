655c5b13893a7231692d50aad8ddd0bfb94909cf
Mateus Bez Fontana <mateus.bezfontana@gmail.com>
2020-09-26 09:07:03 -0300

    refactor: implement Liskov SP

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,15 +1,18 @@
 import { EntityRepository, Repository } from 'typeorm';
 
+import IAppointmentsRepository from '@modules/appointments/repositories/iAppointmentsRepository';
+
 import Appointment from '../entities/Appointment';
 
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> {
-  public async findByDate(date: Date): Promise<Appointment | null> {
+class AppointmentsRepository extends Repository<Appointment>
+  implements IAppointmentsRepository {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
     const findAppointment = await this.findOne({
       where: { date },
     });
 
-    return findAppointment || null;
+    return findAppointment;
   }
 }
 
--- src/modules/appointments/repositories/iAppointmentsRepository.ts
+++ src/modules/appointments/repositories/iAppointmentsRepository.ts
@@ -1,1 +1,7 @@
-interface Test {}
+import Appointment from '../infra/typeorm/entities/Appointment';
+
+interface IAppointmentsRepository {
+  findByDate(date: Date): Promise<Appointment | undefined>;
+}
+
+export default IAppointmentsRepository;
